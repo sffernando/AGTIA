d95c19ea24838253d8f63b916997dbc1adbc7a99
fernando <sucrezhang@qq.com>
2017-02-06 16:50:05 +0800

    bug fixed in qqcute view oc version

--- AGTIA_Objective-C/QQCuteView/QQCuteView/Classes/QQCuteView.m
+++ AGTIA_Objective-C/QQCuteView/QQCuteView/Classes/QQCuteView.m
@@ -39,18 +39,17 @@
     CAShapeLayer *shapeLayer;
 }
 
-- (id)initWithPoint:(CGPoint)point superView:(UIView *)sView {
+-(id)initWithPoint:(CGPoint)point superView:(UIView *)view{
     self = [super initWithFrame:CGRectMake(point.x, point.y, self.bubbleWidth, self.bubbleWidth)];
-    if (self) {
+    if(self){
         initialPoint = point;
-        self.containerView = sView;
+        self.containerView = view;
         [self.containerView addSubview:self];
     }
     return self;
 }
 
-- (instancetype)initWithFrame:(CGRect)frame
-{
+- (id)initWithFrame:(CGRect)frame{
     self = [super initWithFrame:frame];
     if (self) {
         [self setUp];
@@ -58,7 +57,8 @@
     return self;
 }
 
-- (void)drawRect {
+
+-(void)drawRect{
     x1 = backView.center.x;
     y1 = backView.center.y;
     x2 = self.frontView.center.x;
@@ -69,7 +69,7 @@
     if (centerDistance == 0) {
         cosDigree = 1;
         sinDigree = 0;
-    } else {
+    }else {
         cosDigree = (y2 - y1)/centerDistance;
         sinDigree = (x2 - x1)/centerDistance;
     }
@@ -85,13 +85,13 @@
     pointP = CGPointMake(pointB.x + (centerDistance / 2)*sinDigree, pointB.y + (centerDistance / 2)*cosDigree);
     
     backView.center = oldBackViewCenter;
-    backView.bounds = CGRectMake(0, 0, r1*2, r2*2);
+    backView.bounds = CGRectMake(0, 0, r1*2, r1*2);
     backView.layer.cornerRadius = r1;
     
     cutePath = [UIBezierPath bezierPath];
     [cutePath moveToPoint:pointA];
     [cutePath addQuadCurveToPoint:pointD controlPoint:pointO];
-    [cutePath moveToPoint:pointC];
+    [cutePath addLineToPoint:pointC];
     [cutePath addQuadCurveToPoint:pointB controlPoint:pointP];
     [cutePath moveToPoint:pointA];
 
@@ -102,17 +102,17 @@
     }
 }
 
-- (void)setUp {
+-(void)setUp{
     shapeLayer = [CAShapeLayer layer];
     self.backgroundColor = [UIColor clearColor];
     self.frontView = [[UIView alloc] initWithFrame:CGRectMake(initialPoint.x, initialPoint.y, self.bubbleWidth, self.bubbleWidth)];
     
-    r2 = self.frontView.bounds.size.width/2;
+    r2 = self.frontView.bounds.size.width / 2;
     self.frontView.layer.cornerRadius = r2;
     self.frontView.backgroundColor = self.bubbleColor;
-
-    backView = [[UIView alloc] initWithFrame:self.frontView.frame];
-    r1 = backView.bounds.size.width/2;
+    
+    backView = [[UIView alloc]initWithFrame:self.frontView.frame];
+    r1 = backView.bounds.size.width / 2;
     backView.layer.cornerRadius = r1;
     backView.backgroundColor = self.bubbleColor;
     
@@ -120,53 +120,56 @@
     self.bubbleLabel.frame = CGRectMake(0, 0, self.frontView.bounds.size.width, self.frontView.bounds.size.height);
     self.bubbleLabel.textColor = [UIColor whiteColor];
     self.bubbleLabel.textAlignment = NSTextAlignmentCenter;
-
+    
     [self.frontView insertSubview:self.bubbleLabel atIndex:0];
-
+    
     [self.containerView addSubview:backView];
     [self.containerView addSubview:self.frontView];
-
+    
     x1 = backView.center.x;
     y1 = backView.center.y;
     x2 = self.frontView.center.x;
     y2 = self.frontView.center.y;
-
+    
     pointA = CGPointMake(x1-r1,y1);   // A
     pointB = CGPointMake(x1+r1, y1);  // B
     pointD = CGPointMake(x2-r2, y2);  // D
     pointC = CGPointMake(x2+r2, y2);  // C
     pointO = CGPointMake(x1-r1,y1);   // O
     pointP = CGPointMake(x2+r2, y2);  // P
-//
+    
     oldBackViewFrame = backView.frame;
     oldBackViewCenter = backView.center;
-//
+    
     backView.hidden = YES;//为了看到 frontView 的气泡晃动效果，需要暂时隐藏 backView
     [self addAniamtionLikeGameCenterBubble];
-//
+    
     UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(handleDragGesture:)];
     [self.frontView addGestureRecognizer:pan];
 }
 
-- (void)handleDragGesture:(UIPanGestureRecognizer *)ges {
+
+-(void)handleDragGesture:(UIPanGestureRecognizer *)ges{
     CGPoint dragPoint = [ges locationInView:self.containerView];
+    
     if (ges.state == UIGestureRecognizerStateBegan) {
         backView.hidden = NO;
         fillColorForCute = self.bubbleColor;
         [self RemoveAniamtionLikeGameCenterBubble];
-    } else if (ges.state == UIGestureRecognizerStateChanged) {
+    }else if (ges.state == UIGestureRecognizerStateChanged){
         self.frontView.center = dragPoint;
-        if (r1 < 6) {
+        if (r1 <= 6) {
             fillColorForCute = [UIColor clearColor];
             backView.hidden = YES;
             [shapeLayer removeFromSuperlayer];
         }
         [self drawRect];
-    } else if (ges.state == UIGestureRecognizerStateEnded || ges.state ==UIGestureRecognizerStateCancelled || ges.state == UIGestureRecognizerStateFailed) {
+    } else if (ges.state == UIGestureRecognizerStateEnded || ges.state ==UIGestureRecognizerStateCancelled || ges.state == UIGestureRecognizerStateFailed){
+        
         backView.hidden = YES;
         fillColorForCute = [UIColor clearColor];
         [shapeLayer removeFromSuperlayer];
-        [UIView animateWithDuration:0.5 delay:0.0 usingSpringWithDamping:0.4 initialSpringVelocity:0.0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        [UIView animateWithDuration:0.5 delay:0.0f usingSpringWithDamping:0.4f initialSpringVelocity:0.0f options:UIViewAnimationOptionCurveEaseInOut animations:^{
             self.frontView.center = oldBackViewCenter;
         } completion:^(BOOL finished) {
             if (finished) {
@@ -174,7 +177,10 @@
             }
         }];
     }
+    
 }
+
+
 //---- 类似GameCenter的气泡晃动动画 ------
 
 -(void)addAniamtionLikeGameCenterBubble {
