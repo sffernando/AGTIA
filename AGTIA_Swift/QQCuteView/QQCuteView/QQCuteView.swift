710fa097874b997e1042dff755db6c9b4fd96e8f
fernando <sucrezhang@qq.com>
2017-02-07 09:09:54 +0800

    init project foe qqcuteview with swift

---
+++ AGTIA_Swift/QQCuteView/QQCuteView/QQCuteView.swift
@@ -0,0 +1,36 @@
+//
+//  QQCuteView.swift
+//  QQCuteView
+//
+//  Created by fernando on 2017/2/6.
+//  Copyright © 2017年 fernando. All rights reserved.
+//
+
+import UIKit
+
+//struct BubbleOptions {
+//    var text: String = ""
+//    var bubbleWidth: CGFloat = 0.0
+//    var viscosity: CGFloat = 0.0
+//    var bubbleColor: UIColor = UIColor.whiteColor()
+//}
+
+struct BubbleOptions {
+    var text: String = ""
+    var bubbleWidth: CGFloat = 0.0
+    var viscosity: CGFloat = 0.0
+    var bubbleColor: UIColor = UIColor.white
+}
+
+
+class QQCuteView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
