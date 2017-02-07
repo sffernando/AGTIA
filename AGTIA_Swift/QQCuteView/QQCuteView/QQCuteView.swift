af363bb584b624bd3f90ac6063c7c8314d6301e0
fernando <sucrezhang@qq.com>
2017-02-07 10:38:22 +0800

    init methods

--- AGTIA_Swift/QQCuteView/QQCuteView/QQCuteView.swift
+++ AGTIA_Swift/QQCuteView/QQCuteView/QQCuteView.swift
@@ -8,13 +8,6 @@
 
 import UIKit
 
-//struct BubbleOptions {
-//    var text: String = ""
-//    var bubbleWidth: CGFloat = 0.0
-//    var viscosity: CGFloat = 0.0
-//    var bubbleColor: UIColor = UIColor.whiteColor()
-//}
-
 struct BubbleOptions {
     var text: String = ""
     var bubbleWidth: CGFloat = 0.0
@@ -22,15 +15,55 @@ struct BubbleOptions {
     var bubbleColor: UIColor = UIColor.white
 }
 
-
 class QQCuteView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var frontView: UIView?
+    var bubbleOptions: BubbleOptions {
+        didSet {
+            bubbleLabel.text = bubbleOptions.text
+        }
     }
-    */
+    
+    private var bubbleLabel: UILabel!
+    private var containerView: UIView!
 
+    private var cutePath: UIBezierPath!
+    private var fillColorForCute: UIColor!
+    private var animator: UIDynamicAnimator!
+    private var snap: UISnapBehavior!
+    private var backView: UIView!
+    private var shapeLayer: CAShapeLayer!
+    
+    private var r1: CGFloat = 0.0
+    private var r2: CGFloat = 0.0
+    private var x1: CGFloat = 0.0
+    private var y1: CGFloat = 0.0
+    private var x2: CGFloat = 0.0
+    private var y2: CGFloat = 0.0
+    private var centerDistance: CGFloat = 0.0
+    private var cosDigree: CGFloat = 0.0
+    private var sinDigree: CGFloat = 0.0
+    
+    private var pointA = CGRect.zero
+    private var pointB = CGRect.zero
+    private var pointC = CGRect.zero
+    private var pointD = CGRect.zero
+    private var pointO = CGRect.zero
+    private var pointP = CGRect.zero
+    
+    private var initialPoint: CGPoint = CGPoint.zero
+    private var oldBackViewFrame: CGRect = CGRect.zero
+    private var oldBackViewCenter: CGPoint = CGPoint.zero
+
+    init(point: CGPoint, superView: UIView, options: BubbleOptions) {
+        bubbleOptions = options
+        super.init(frame: CGRect(x: point.x, y: point.y, width: options.bubbleWidth, height: options.bubbleWidth))
+        initialPoint = point
+        containerView = superView
+        containerView.addSubview(self)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
