37626652410a3e091aa124781d9b4abeb886e659
fernando <sucrezhang@qq.com>
2017-02-10 09:18:22 +0800

    LiquidUtil

---
+++ AGTIA_Swift/LiquidLoader/LiquidLoader/CircularGradientLayer.swift
@@ -0,0 +1,13 @@
+//
+//  CircularGradientLayer.swift
+//  LiquidLoader
+//
+//  Created by fernando on 2017/2/10.
+//  Copyright © 2017年 fernando. All rights reserved.
+//
+
+import UIKit
+
+class CircularGradientLayer: CALayer {
+
+}
---
+++ AGTIA_Swift/LiquidLoader/LiquidLoader/LiquidUtil.swift
@@ -0,0 +1,53 @@
+//
+//  LiquidUtil.swift
+//  LiquidLoader
+//
+//  Created by fernando on 2017/2/10.
+//  Copyright © 2017年 fernando. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+func withBezier(_ f: (UIBezierPath) -> ()) -> UIBezierPath {
+    let bezierPath = UIBezierPath()
+    f(bezierPath)
+    bezierPath.close()
+    return bezierPath
+}
+
+func withStroke(_ bezierPath: UIBezierPath, color: UIColor, f: ()-> ()) {
+    color.setStroke()
+    f()
+    bezierPath.stroke()
+}
+
+func withFill(_ bezierPath: UIBezierPath, color: UIColor, f: ()-> ()) {
+    color.setFill()
+    f()
+    bezierPath.fill()
+}
+
+class CGMath {
+    static func radToDeg(_ rad: CGFloat) -> CGFloat {
+        return rad * 180 / CGFloat(M_PI)
+    }
+    
+    static func degToRad(_ deg: CGFloat) -> CGFloat {
+        return deg * CGFloat(M_PI) / 180
+    }
+    
+    static func circlePoint(_ center: CGPoint, radius: CGFloat, rad: CGFloat) -> CGPoint {
+        let x = center.x + radius * cos(rad)
+        let y = center.y + radius * sin(rad)
+        return CGPoint(x: x, y: y)
+    }
+    
+    static func lineSpace(_ from: CGFloat, to: CGFloat, n: Int) -> [CGFloat] {
+        var values: [CGFloat] = []
+        for i in 0..<n {
+            values.append((to - from) * CGFloat(i) / CGFloat(n - 1) + from)
+        }
+        return values
+    }
+}
