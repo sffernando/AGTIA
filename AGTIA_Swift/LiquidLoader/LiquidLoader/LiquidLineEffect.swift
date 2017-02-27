e31c3d1d85ddf8df60ada8f95a46e6782c139189
fernando <sucrezhang@qq.com>
2017-02-27 15:31:55 +0800

    LiquidLineEffect

---
+++ AGTIA_Swift/LiquidLoader/LiquidLoader/LiquidLineEffect.swift
@@ -0,0 +1,69 @@
+//
+//  LiquidLineEffect.swift
+//  LiquidLoader
+//
+//  Created by fernando on 2017/2/27.
+//  Copyright © 2017年 fernando. All rights reserved.
+//
+
+import UIKit
+
+class LiquidLineEffect: LiquidLoadEffect {
+//    var circleInter: CGFloat!
+    var circleInter: CGFloat!
+    
+    override func setupShape() -> [LiquittableCircle] {
+        return Array(0..<numberOfCircles).map{ i in
+            return LiquittableCircle(center: CGPoint(x: self.circleInter + self.circleRadius + CGFloat(i) * (self.circleInter + 2 * self.circleRadius), y: self.loader.frame.height * 0.5), radius: self.circleRadius, color: self.color, growColor: self.growColor)
+        }
+    }
+    
+    override func movePosition(_ key: CGFloat) -> CGPoint {
+        if loader != nil {
+            return CGPoint(
+                x: (circles.last!.frame.rightBottom.x + circleInter) * sineTransform(key),
+                y: loader.frame.height * 0.5
+            )
+        } else {
+            return CGPoint.zero;
+        }
+    }
+    
+    func sineTransform(_ key: CGFloat) -> CGFloat {
+        return sin(key * CGFloat(M_PI)) * 0.5 + 0.5
+    }
+    
+    override func update() {
+        switch key {
+        case 0.0...2.0:
+            key += 2.0/(duration*60)
+        default:
+            key = 0.0
+        }
+    }
+
+    override func willSetup() {
+        if circleRadius == nil {
+            circleRadius = loader.frame.width * 0.05
+        }
+        
+        self.circleInter = (loader.frame.width - 2 * circleRadius * 5) / 6
+        self.engine = SimpleCircleLiquidEngine(radiusThresh: self.circleRadius, angleThresh: 0.2)
+        let moveCircleRadius = circleRadius * moveScale
+        self.moveCircle = LiquittableCircle(center: CGPoint(x: 0, y: loader.frame.height * 0.5), radius: moveCircleRadius, color: color, growColor: growColor)
+    }
+
+    override func resize() {
+        circles.map { circle in
+            return (circle, circle.center.minus(self.moveCircle!.center).length())
+            }.each { (circle, distance) in
+                let normalized = 1.0 - distance / (self.circleRadius + self.circleInter)
+                if normalized > 0.0 {
+                    circle.radius = self.circleRadius + (self.circleRadius * self.circleScale - self.circleRadius) * normalized
+                } else {
+                    circle.radius = self.circleRadius
+                }
+        }
+    }
+
+}
